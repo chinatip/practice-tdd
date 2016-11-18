9ae91cac0273876e3164e798f635dd3997268c3f
Chocolate <namtan8888@gmail.com>
2016-11-18 13:02:46 +0700

    Updated getFibo fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
-
+const fib = [0, 1, 1]
 export const getFibo = (n) => { 
-	if(n == 1 || n == 2) return 1
-	if(n == 0) return 0
-	return getFibo(n-1) + getFibo(n-2)
+	if(!fib[n]) {
+		fib[n] = getFibo(n-1) + getFibo(n-2)
+	}
+	return fib[n]
 }
